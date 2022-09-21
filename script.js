9a6c3c4a54f9925f28a65be2325571c7ce046de5
Daniele Pellegrini <danny.m.pellegrini@gmail.com>
2022-09-21 16:06:53 -0400

    Changed the structure of Deck Class

--- script.js
+++ script.js
@@ -27,39 +27,38 @@ class Card {
 
 class Deck {
 	constructor (){
-		this.suits = ['hearts','spades','clubs','diamonds']
-		this.scores = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-		this.cards = []
+		this.deck = []
 		this.makeDeck()
 	}
 	makeDeck() {
-		for (const suit of this.suits) {
-			for (const score of this.scores) {
+		const suits = ['hearts','spades','clubs','diamonds']
+		const scores = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+		for (const suit of suits) {
+			for (const score of scores) {
 				if (score === 1){
 					const card = new Card(suit,"Ace",score)
-					this.cards.push(card)
+					this.deck.push(card)
 				}
 				else if (score === 11){
 					const card = new Card(suit,"Jack",score)
-					this.cards.push(card)
+					this.deck.push(card)
 				}
 				else if (score === 12){
 					const card = new Card(suit,"Queen",score)
-					this.cards.push(card)
+					this.deck.push(card)
 				}
 				else if (score === 13){
 					const card = new Card(suit,"King",score)
-					this.cards.push(card)
+					this.deck.push(card)
 				}
 				else {
 					const card = new Card(suit,`${score}`,score)
-					this.cards.push(card)
+					this.deck.push(card)
 				}
 			}
 		}
 	}
-	drawCard = function (){
-		let cardDrawn = Math.floor(Math.random()*51)
-		console.log(this.cards[cardDrawn])
-	}
 }
+
+const newDeck = new Deck
+console.log(newDeck)
